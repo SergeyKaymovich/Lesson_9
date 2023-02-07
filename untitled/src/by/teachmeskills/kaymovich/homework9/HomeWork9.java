0e486e58675637c34998f68796d7003336c33ef7
Sergey Kaymovich <Sergey.kaymovich@mail.ru>
2023-02-07 20:17:01 +0300

    chabge HomeWork9

---
+++ untitled/src/by/teachmeskills/kaymovich/homework9/HomeWork9.java
@@ -0,0 +1,47 @@
+package by.teachmeskills.kaymovich.homework9;
+
+import by.teachmeskills.kaymovich.homework9.utils.StringUtils;
+
+import java.util.Scanner;
+
+import static by.teachmeskills.kaymovich.homework9.utils.StringUtils.print;
+import static by.teachmeskills.kaymovich.homework9.utils.StringUtils.println;
+
+public class HomeWork9 {
+    public static void main(String[] args) {
+        String info = """  
+                              Select a task by number:
+                1. Task IsBlank.
+                2. Task isRussian.
+                3. Task contains.\
+                """;
+        System.out.println(info);
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+//        char[] string1 = scanner.nextLine().toCharArray();
+        switch (input) {
+            case (1) -> {
+                System.out.println("Input string");
+                char[] string = scanner.nextLine().toCharArray();
+                print(string);
+                System.out.println(". -  This is blank?  -  " + StringUtils.isBlank(string));
+            }
+            case (2) -> {
+                System.out.println("Input string");
+                char[] string = scanner.nextLine().toCharArray();
+                print(string);
+                System.out.println("The array consists of characters of the Russian alphabet?");
+                System.out.println(StringUtils.isRussian(string));
+            }
+            case (3) -> {
+                System.out.println("Input string");
+                char[] string = scanner.nextLine().toCharArray();
+                println(string);
+                char[] string2 = scanner.nextLine().toCharArray();
+                println(string);
+                System.out.println("Do the array strings match?");
+                System.out.println(StringUtils.contains(string, string2));
+            }
+        }
+    }
+}
---
+++ untitled/src/by/teachmeskills/kaymovich/homework9/utils/StringUtils.java
@@ -0,0 +1,51 @@
+package by.teachmeskills.kaymovich.homework9.utils;
+
+public class StringUtils {
+    public static void print(char[] array) {
+        for (char sym : array) {
+            System.out.print(sym);
+        }
+    }
+
+    public static void println(char[] array) {
+        print(array);
+        System.out.print('\n');
+    }
+
+    public static boolean isBlank(char[] array) {
+        for (char sym : array) {
+            if (!Character.isWhitespace(sym)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isRussian(char... inputArray) {
+        for (char sym : inputArray) {
+            if ((sym < 'А' || sym > 'Я') && (sym < 'а' || sym > 'я') && (sym != 'Ё' && sym != 'ё')) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean contains(char[] arraysym1, char[] arraysym2) {
+        int sum = 0;
+        if (arraysym1.length > arraysym2.length) {
+            throw new IllegalArgumentException("Incorrect value.");
+        }
+        for (int i = 0; i < arraysym1.length - 1; i++) {
+            for (int j = 0; j < arraysym2.length; j++) {
+                if (arraysym1[i + j] != arraysym2[j]) {
+                    break;
+                } else {
+                    sum++;
+                }
+            }
+            if (sum == arraysym2.length)
+                return true;
+        }
+        return false;
+    }
+}
